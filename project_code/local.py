559db9d7be5ead7373bd6e44a789332c577c07cc
ilfeld <ilfeld@gmail.com>
2014-03-26 03:44:00 +0000

    updated python script with url links
    and formatted font size according to popularity

--- project_code/local.py
+++ project_code/local.py
@@ -17,7 +17,7 @@ MAIN_PAGE_HTML = """\
   <body>
     <form action="/sign" method="post">
       <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Enter Address"></div>
+      <div><input type="submit" value="Enter address to discover popular wikipedia pages in its vicinity"></div>
     </form>
   </body>
 </html>
@@ -33,14 +33,13 @@ class MainPage(webapp2.RequestHandler):
 class Guestbook(webapp2.RequestHandler):
 
     def post(self):
-        self.response.write('<!doctype html><html><body>You wrote:  ')
-        self.response.write(cgi.escape(self.request.get('content')))
-        self.response.write('<br><br>The resulting outupt is: ')
+        self.response.write('</strong><a href="/">CLICK HERE TO TRY ANOTHER ADDRESS</a><br>')
+        self.response.write('<!doctype html><html><body><br>You wrote: <i>"')
+        self.response.write(cgi.escape(self.request.get('content'))+'"</i>')
         address=str(cgi.escape(self.request.get('content')))
         g=  addressToGPS(address)
-        self.response.write('<blockquote>The longitude and lattitude are: <strong>%s</blockquote>' %g)
-        self.response.write('<blockquote>%s</blockquote>' %GetWiki(g))
-        self.response.write('</strong><br><a href="/">CLICK HERE TO TRY AGAIN</a></body></html>')
+        self.response.write('   The resulting longitude and lattitude are: <strong>%s   ' %g)
+        self.response.write('</strong>Here are the most popular sites nearby (with their Wikipedia popularity determining their size)<br><strong>%s</body></html>' %GetWiki(g))
 
 
 application = webapp2.WSGIApplication([
@@ -97,7 +96,7 @@ def GetWiki(gps):
     query="?action=query&format=json&generator="
     limit="limit=12"
     ##gps=[ 32.070278, 34.794167] Electra Tower
-    radius="1500"
+    radius="9000"
     location="geosearch&ggscoord="+lattitude+"%7C"+longitude+"&ggsradius="+radius+"&ggs"
     images_query = 'https://en.wikipedia.org/w/api.php?action=query&prop=images&format=json&pageids='
     TOP_COUNT = 7
@@ -110,23 +109,63 @@ def GetWiki(gps):
         p=res.get('query', {}).get('pages')
         if p is None:
             return 'No results found this time'+str(res)+str(gps)+longitude
-
+        ordered=""
+        size=7
         final_pages = {}
         pages_scores = {}
+        listpages=[]
+        statlist=[]
         for page_id, page in p.iteritems():
-            # Get the page's popularity
-##            print page_id, page['title'], tcount(page['title'])
-            pages_scores[page_id] = [page['title'], tcount(page['title'])]
-
-        sorted_scores = sorted(pages_scores.items(), key=lambda e: e[1][1], reverse=True)[:TOP_COUNT]
-        res = dict(sorted_scores)
-        for k in res.keys():
-            res[k] = res[k][0]
-
-        ordered="</strong>The most popular sites nearby: <strong>"            
-        for k in res:
-            ordered= ordered+res[k]+". "
+            # Get the page's popularity and normalize it
+            stat=tcount(page['title'])
+            normalized=round((stat)/2000)
+            if normalized >15:
+                normalized=15
+            if normalized < 1:
+                normalized=1
+            pages_scores[page_id] = [page['title'], normalized]
+            listpages.append([page['title'],page_id,stat])
+            statlist.append(stat)
+        avgstat=sum(statlist)/len(statlist)
+        minim=min(statlist)
+        maxim=max(statlist)
+        second=avgstat-minim
+        beforelast=maxim-avgstat
+        s=5
+        for x in listpages:
+            pageStat=x[2]
+            if pageStat>maxim-10:
+                s=10
+            else:
+                if pageStat<minim+10:
+                    s=1
+                else:
+                    if pageStat>minim and pageStat<avgstat:
+                        s=5
+                    else:
+                        if pageStat>avgstat and pageStat<maxim:
+                            s=8
+                        else:
+                            if pageStat==avgstat:
+                                s=7
+            url="http://en.wikipedia.org/wiki?curid="+str(x[1])
+            ordered= ordered+"<font size='"+str(s)+"'><a href="+"'"+url+"'"+'>'+str(x[0])+"</a>  </font>"
+        for x in range(0,7):
+            ordered=ordered+ordered
         return ordered
+
+
+##        sorted_scores = sorted(pages_scores.items(), key=lambda e: e[1][1], reverse=True)[:TOP_COUNT]
+##        res = dict(sorted_scores)
+##        for k in res.keys():
+##            res[k] = res[k][0]
+##
+##        ordered="</strong><font size='4'>The most popular sites nearby (with popularity determining size)<br> </font><strong>"
+##        size=7
+##        for k in res:
+##            ordered= ordered+"<font size='"+str(size)+"'>"+res[k]+". </font>"
+##            size=size-1
+##        return ordered+str(sorted_scores)
     
 
 
